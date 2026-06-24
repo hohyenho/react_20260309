d64d90d4f06309e0f49a423f0bc4b123d5ea6780
hohyenho <75921113+hohyenho@users.noreply.github.com>
2026-06-24 10:40:15 +0900

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,37 @@
 import React, { useState } from 'react'
+const headerNav = [
+    {
+       title:"intro",
+       url:"/",
+    },
+    {
+        title:"skill",
+        url:"/",
+    },
+    {
+        title:"site",
+        url:"/",
+    },
+    {
+        title:"port",
+        url:"/",
+    },
+    {
+        title:"contact",
+        url:"/",
+    },
+    
+]
+
 
 const Header = () => {
 
     const [show, setShow] = useState(false);
-    //useState() : 컨퍼넌트의 상태를 관리해주는 훅
-    //false : 초기값
-    //show : 상태를 만들고, 초기값을 false로 지정
-    //setShow : 이 상태값을 바꾸는 함수(상태를 업그레이드 한 상태)
+    //show : false  
 
-    //들어온값의 반대값으로 바꿔주는 함수
-    const toggleMenu = ()=> {
-        setShow((pShow) => !pShow);
-        //! :반대값 참->거짓  거짓 -> 참
+    const toggleMenu = ()=>{
+        setShow((pShow) => !pShow)
+        // 들어온 입력값을 0 -> 1 , 1 -> 0 로변경해주는 함수
     }
 
 
@@ -21,21 +41,24 @@ const Header = () => {
                 <h1 className="header_logo">
                     <a href="/">portfolio <em>react.js</em></a>
                 </h1>
-                <nav className={`header_nav ${show ? "show" :""}`} role='navigation' aria-label='800이상 메뉴'>
+                <nav className={`header_nav ${show ? "show":""}`} role='navigation' aria-label='800이상 메뉴'>
                     <ul>
-                        <li><a href="/">intro</a></li>
-                        <li><a href="/">skill</a></li>
-                        <li><a href="/">site</a></li>
-                        <li><a href="/">port</a></li>
-                        <li><a href="/">contact</a></li>
+                        {headerNav.map((nav,key)=>(
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
                 <nav 
                     className="header_nav_m" 
                     id='headerToggle'
                     role='button'
-                    tabIndex={0}
-                    onClick={toggleMenu}
+                    tabIndex={0}       
+                    onClick={toggleMenu}    
+                    //클릭했을때 toggleMenu 함수를 실행해라.
+                    aria-expanded={show ? "true":"false"}
+                    
                 >
                     <span></span>
                 </nav>
